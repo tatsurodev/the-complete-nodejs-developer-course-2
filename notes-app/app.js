e020a37db73d1459a53ca746a5f303404e8b04f4
tatsurodev <contacts@tatsuro.dev>
2023-10-15 00:56:19 +0900

    npm moduleのimport 11

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,9 @@
+// npm moduleはmodule名でimport
+const validator = require('validator')
 // 自作moduleはrelative pathでimpport
 const getNotes = require('./notes.js')
+
 const msg = getNotes()
 console.log(msg)
+
+console.log(validator.isURL('https//mead.io'))
