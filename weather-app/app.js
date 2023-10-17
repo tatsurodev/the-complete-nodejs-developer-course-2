ea885f9e5fd0e8eb24d7a76424975c10906cebc3
tatsurodev <contacts@tatsuro.dev>
2023-10-18 01:49:55 +0900

    非同期処理の基礎 29

---
+++ weather-app/app.js
@@ -0,0 +1,17 @@
+console.log('Starting')
+
+setTimeout(() => {
+  console.log('2 Second Timer')
+}, 2000)
+
+setTimeout(() => {
+  console.log('0 Second Timer')
+}, 0)
+
+console.log('Stopping')
+
+// result
+// Starting
+// Stopping
+// 0 Second Timer
+// 2 Second Timer
