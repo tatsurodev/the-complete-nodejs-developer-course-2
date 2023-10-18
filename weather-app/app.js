0344eaa4bc1479584913123228dcfd2fa0d31d8b
tatsurodev <contacts@tatsuro.dev>
2023-10-19 05:58:20 +0900

    nodeからhttp request 31

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv')
+const request = require('request')
 
 // 開発環境(development or production)によって.envを使い分け
 // NODE_ENV=development node app.js
@@ -9,3 +10,10 @@ dotenv.config({
 
 // .envを値を使用するには、process.env.KEY
 // console.log(process.env.TEST)
+
+const url = `http://api.weatherstack.com/current?access_key=${process.env.API_KEY}&query=37.8267,-122.4233`
+
+request({ url }, (error, response) => {
+  const data = JSON.parse(response.body)
+  console.log(data.current)
+})
